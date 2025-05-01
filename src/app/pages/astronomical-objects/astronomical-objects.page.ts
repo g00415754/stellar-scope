2ff3d3ef0d285736400a4a783a5a6a84295cf529
g00415754 <dianakudrjavceva13@gmail.com>
2025-05-01 12:25:09 +0100

    uses stellariumweb.org

--- src/app/pages/astronomical-objects/astronomical-objects.page.ts
+++ src/app/pages/astronomical-objects/astronomical-objects.page.ts
@@ -1,21 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-import { IonCardContent, IonCardTitle, IonCardHeader, IonCard } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-astronomical-objects',
   templateUrl: './astronomical-objects.page.html',
   styleUrls: ['./astronomical-objects.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonCardContent,IonCardTitle,IonCardHeader,IonCard, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, CommonModule, FormsModule]
 })
-export class AstronomicalObjectsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AstronomicalObjectsPage {
 
 }
+
